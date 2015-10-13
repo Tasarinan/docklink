7bf6eb393eeb71cbb9768bd1ea29b81119b25815
HB <jben@sohu.com>
2015-10-13 18:01:38 +0800

    Parse done and test application done

--- app/src/main/java/swrc/io/docklink/activities/MainActivity.java
+++ app/src/main/java/swrc/io/docklink/activities/MainActivity.java
@@ -1,9 +1,13 @@
 package swrc.io.docklink.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 import com.parse.ParseAnalytics;
 import com.parse.ParseObject;
@@ -11,7 +15,7 @@ import com.parse.ParseObject;
 import swrc.io.docklink.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "swrc.io.docklink.message";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+    public void sendMessage(View view){
+        Intent intent = new Intent(this,DisplayMessageActivity.class);
+        EditText editText =(EditText)findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
     }
 
     @Override
